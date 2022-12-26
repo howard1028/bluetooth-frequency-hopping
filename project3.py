23e47c9e8fd6b58a80ceb7c510520e935e3e80ff
howard1028 <993004@gmail.com>
2022-12-26 11:45:51 +0800

    small change

--- project3.py
+++ project3.py
@@ -1,6 +1,5 @@
 import random
 import matplotlib.pyplot as plt
-import pandas as pd
 
 channel = 79
 channel_prob = [] #channel出現次數
@@ -13,11 +12,9 @@ time = 0 #第幾次選channel
 threshold = 0.1 #出現超過此機率的算bad channel
 collision_total = [] #每個channel碰撞次數
 total_bad_channel = 0 #bad channel總數
-channel_with_noise = []
-channel_without_noise = []
-bad = [] #bad channel id
 
-bad_channel = [] #if bad 則 =1
+
+bad_channel = [] #if bad則=最近的good channel，if good則=0
 hopping_sequence = []
 
 
@@ -25,66 +22,142 @@ hopping_sequence = []
 def f3(L):
   return [e for e in set(L) if L.count(e) > 1]
 
-#####################################################前4秒
+##################################################### 前4秒
 temp = []
 for i in range(79):
     temp.append(i)
-print(temp)
+# print(temp)
 
 
 #每個device的hopping sequence
 for i in range(device_num):
     a=random.sample(temp,79)
-    hopping_sequence.append(a)
-print(hopping_sequence)
-
-
-
+    hopping_sequence+=[a]
+print("hopping sequence=",hopping_sequence)
 
 for i in range(device_num):
     prev_channel.append(0)
-# print(prev_channel)
 
+#各channel碰撞次數
 for i in range(channel):
     collision_total.append(0)
 
+count=0
 while True:
+    for i in range(device_num):
+        if len(hopping_sequence[i]) != 0:
+            prev_channel[i] = hopping_sequence[i][count]
+    count += 1
 
-    for i in range(hopping_sequence[0]):
-        prev_channel[i]=hopping_sequence[]
+    # print(len(hopping_sequence[0]),len(hopping_sequence[1]))
+    print("\n",time+1,":",prev_channel)   
 
     #各channel碰撞次數
     # print(f3(prev_channel))
-    for i in range(len(hopping_sequence[0])):
+    for i in range(len(f3(prev_channel))):
         collision_total[f3(prev_channel)[i]-1] += 1
-        
+    print("collision_total=",collision_total)
+
     #總碰撞次數
     collision_time += len(f3(prev_channel))
 
     time += 1    
-    if time == hop_per_sec*sec:
+    if time == channel:
         break
 # print("collision_probability =",collision_time/(hop_per_sec*sec))
 
 #換成機率
 for i in range(len(collision_total)):
-    collision_total[i] /= hop_per_sec*sec
-print("collision_total",collision_total)
+    collision_total[i] /= channel
+# print("collision_total",collision_total)
+
+
+######################################################## 後26秒
+
+sec = 26
+P = [] #該threshold平均碰撞機率
+
+for i in range(channel):
+    bad_channel.append([0,0,0,0,0,0,0,0,0])
+
+# print("bad_channel=",bad_channel)
+
+for i in range(channel):
+    for j in range(9):
+        threshold = (j+1)/10
+        # channel機率 > threshold是bad channel
+        if (collision_total[i] >= threshold):
+            bad_channel[i][j] = i+1
+for j in range(9):            
+    for i in range(channel):
+        threshold = (j+1)/10
+        #bad channel，向左右找最近的good channel
+        if (bad_channel[i][j] != 0):
+            p1 = i
+            p2 = i
+            count1=0
+            count2=0
+            while p2 < channel:
+                if bad_channel[p2][j] == 0:
+                    break
+                count2 += 1
+                p2 += 1
+            while p1 >= 0:
+                if bad_channel[p1][j] == 0:
+                    break
+                count1 += 1
+                p1 -= 1
+            if p1 == -1 and p2 != 79:
+                bad_channel[i][j] = p2+1
+            if p1 != -1 and p2 == 79:
+                bad_channel[i][j] = p1+1
+            if p1 != -1 and p2 != 79:
+                if count1<count2:
+                    bad_channel[i][j] = p1+1
+                else:
+                    bad_channel[i][j] = p2+1
+
+print("bad_channel=",bad_channel)
+p=[]
+for k in range(9):
+    collision = [0]*79
+    for i in range(sec*hop_per_sec):
+        choice_channel=[0]*79
+        for j in range(device_num):
+            bad_channel[hopping_sequence[j][i%79]][k]
+            hopping_sequence[j][i%79]
+            if bad_channel[hopping_sequence[j][i%79]][k]==0:
+                choice_channel[hopping_sequence[j][i%79]]+=1
+            else:
+                choice_channel[bad_channel[hopping_sequence[j][i%79]][k]-1]+=1
+        for j in range(len(choice_channel)):
+            if choice_channel[j] > 1:
+                collision[j]+=choice_channel[j]
+    #print(collision)
+    sum=0
+    for i in range(len(collision)):
+        sum+=collision[i]
+    sum=sum/sec/hop_per_sec/device_num
+    p+=[sum]
+print(p)
 
 
 
 
-# C = []
-# for i in range(9):
-#     C.append((i+1)/10)
-# # print(C)
+
+
+C = []
+for i in range(9):
+    C.append((i+1)/10)
+# print(C)
 
 
 
 
-# plt.bar(C, bad_total, color='b',width=0.05)
-# plt.xlabel('Threshold ζ') # 設定x軸標題
-# plt.ylabel('Average collision probability') # 設定y軸標題
-# # plt.xticks(C, rotation='vertical') # 設定x軸label以及垂直顯示
-# plt.title('device = 20') # 設定圖表標題
-# plt.show()
+
+plt.bar(C, p, color='b',width=0.05)
+plt.xlabel('Threshold ζ') # 設定x軸標題
+plt.ylabel('Average collision probability') # 設定y軸標題
+# plt.xticks(C, rotation='vertical') # 設定x軸label以及垂直顯示
+plt.title('device = 20') # 設定圖表標題
+plt.show()
--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-bad = ['Bad','Smooth Criminal','Speed Demon','Man in the Mirror','Dirty Diana']
-print(bad.index("Dirty "))
+# bad = ['Bad','Smooth Criminal','Speed Demon','Man in the Mirror','Dirty Diana']
+# print(bad.index("Dirty "))
+
+list1=[1,2,3]
+list2=[]
+list2+=list1
+list1.pop()
+print(list1,list2)
